de6af2d0d64191b4dc8e3b156c5e8cc2743f290d
winterwang <wangwentao0100@163.com>
2026-04-26 15:03:20 +0800

    Initial commit

---
+++ export.sh
@@ -0,0 +1,10 @@
+source /APP/u22/ai_x86/toolshs/setproxy.sh 172.16.31.200 3138
+
+export UV_CACHE_DIR=/media/DISK4TB/personal/winterwang/.cache/uv
+export HF_HOME="/media/DISK4TB/personal/winterwang/.cache/hf"
+
+export HF_ENDPOINT=https://hf-mirror.com
+export HF_TOKEN="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"
+
+export UV_INDEX_URL=https://pypi.tuna.tsinghua.edu.cn/simple
+export UV_PYTHON_INSTALL_MIRROR="https://gh-proxy.com/https://github.com/astral-sh/python-build-standalone/releases/download"
